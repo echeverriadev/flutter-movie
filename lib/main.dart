dba4267cd6e5f6a37bb26d207d5f6f8a1dbb7d43
Carlos Echeverria <echeverriadev@gmail.com>
2020-05-15 13:44:03 -0400

    initial commit

---
+++ lib/main.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_movie/src/pages/home_page.dart';
+ 
+void main() => runApp(MyApp());
+ 
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Movies',
+      initialRoute: '/',
+      routes: {
+        '/' : (BuildContext context) => HomePage(),
+      },
+    );
+  }
+}
